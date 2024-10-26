25bed77e3b9c2907aeee21fd06f839025a291dbf
Amit kumar <amitbhrampuriya@gmail.com>
2024-10-26 16:09:13 +0530

    edit

--- src/app/api/getallgarbage/route.js
+++ src/app/api/getallgarbage/route.js
@@ -6,7 +6,7 @@ import GarbageModel from '../../../Database/schema/garbageSchema';
 export async function GET(req, res) {
     await connectToDatabase();
     try {
-        const data = await GarbageModel.find({status: 'pending'})
+        const data = await GarbageModel.find({})
         return NextResponse.json({ success: true, data: data } , {status: 200});
     } catch (error) {
         console.log(error);
